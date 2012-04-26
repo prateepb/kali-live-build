a1d50a4c32d47c787eb96cd60e254e8020c80947
root <root@repository.kali.org>
2012-04-26 15:28:21 -0400

    Improve build_all.sh so that it actually works
    Do not put use generic environment variables that are in conflict
    with live-build internal variables. Auto-publish the resulting images.

--- build_all.sh
+++ build_all.sh
@@ -2,13 +2,24 @@
 
 set -e
 
-ARCHES="i386 amd64"
-DATE=$(date +%Y%m%d)
+KALI_ARCHES="i386 amd64"
+KALI_VERSION="${VERSION:-daily}"
 
-for ARCH in ARCHES; do
-	lb clean --purge
-	lb config --architecture $ARCH
-	lb build
-	mv binary.iso kali-$ARCH.iso
+cd $(dirname $0)
+
+for KALI_ARCH in $KALI_ARCHES; do
+	lb clean --purge >prepare.log 2>&1
+	lb config --architecture $KALI_ARCH >>prepare.log 2>&1
+	lb build >/dev/null
+	if [ ! -e binary.iso ]; then
+		echo "Build of $KALI_ARCH live image failed" >&2
+		echo "Last 50 lines of the log:" >&2
+		tail -n 50 binary.log >&2
+		exit 1
+	fi
+	mv binary.iso images/kali-$KALI_VERSION-$KALI_ARCH.iso
+	mv binary.log images/kali-$KALI_VERSION-$KALI_ARCH.log
 done
+
+sha1sum images/*.iso >>images/SHA1SUMS
 
