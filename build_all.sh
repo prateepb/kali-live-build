4ca36c8edc0b756307253071923b0b6ffdce8543
Raphaël Hertzog <raphael@freexian.com>
2012-08-13 11:15:54 -0400

    Update live-build's config for version 3.x in wheezy

--- build_all.sh
+++ build_all.sh
@@ -11,13 +11,13 @@ for KALI_ARCH in $KALI_ARCHES; do
 	lb clean --purge >prepare.log 2>&1
 	lb config --architecture $KALI_ARCH >>prepare.log 2>&1
 	lb build >/dev/null
-	if [ ! -e binary.iso ]; then
+	if [ ! -e binary.hybrid.iso ]; then
 		echo "Build of $KALI_ARCH live image failed" >&2
 		echo "Last 50 lines of the log:" >&2
 		tail -n 50 binary.log >&2
 		exit 1
 	fi
-	mv binary.iso images/kali-$KALI_VERSION-$KALI_ARCH.iso
+	mv binary.hybrid.iso images/kali-$KALI_VERSION-$KALI_ARCH.iso
 	mv binary.log images/kali-$KALI_VERSION-$KALI_ARCH.log
 done
 
