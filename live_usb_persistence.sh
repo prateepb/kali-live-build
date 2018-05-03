a643e64fe9b1a7c1ce10c3bb3db3275a506d149b
Prateep Bandharangshi <prateep@anicca.net>
2018-05-03 10:30:42 +0100

    Add DEVICE variable

--- live_usb_persistence.sh
+++ live_usb_persistence.sh
@@ -3,15 +3,17 @@
 # http://docs.kali.org/downloading/kali-linux-live-usb-install
 # http://docs.kali.org/downloading/kali-linux-live-usb-persistence
 
+DEVICE="/dev/sdb"
+
 if [ "$1" != "fuckyes" ]; then
     exit 1
 fi
 
-dosfslabel /dev/sdb2 KALI-BOOT
-dosfslabel /dev/sdb3 KALI-DATA
+dosfslabel ${DEVICE}2 KALI-BOOT
+dosfslabel ${DEVICE}3 KALI-DATA
 
-cryptsetup --verbose --verify-passphrase luksFormat /dev/sdb4
-cryptsetup luksOpen /dev/sdb4 my_usb
+cryptsetup --verbose --verify-passphrase luksFormat ${DEVICE}4
+cryptsetup luksOpen ${DEVICE}4 my_usb
 mkfs.ext3 -L persistence /dev/mapper/my_usb
 e2label /dev/mapper/my_usb persistence
 mkdir -p /mnt/my_usb
@@ -19,4 +21,3 @@ mount /dev/mapper/my_usb /mnt/my_usb
 echo "/ union" > /mnt/my_usb/persistence.conf
 umount /dev/mapper/my_usb
 cryptsetup luksClose /dev/mapper/my_usb
-
